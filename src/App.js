343076939210d34d5fa432c80748a3b5f40f82ce
KReysen <43965763+KReysen@users.noreply.github.com>
2019-02-04 17:33:22 -0700

    Added RoomList component, rendered in App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList.js';
+
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDJECp3CpM5yzRs7yKDXg9RCMacRwe6kfM",
+    authDomain: "bloc-chat-b312c.firebaseapp.com",
+    databaseURL: "https://bloc-chat-b312c.firebaseio.com",
+    projectId: "bloc-chat-b312c",
+    storageBucket: "bloc-chat-b312c.appspot.com",
+    messagingSenderId: "279165312367"
+  };
+  firebase.initializeApp(config);
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Bloc Chat</h1>
+        <RoomList firebase={firebase} />
       </div>
     );
   }
