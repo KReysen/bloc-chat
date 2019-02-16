07cafdcd4d9b6cd9f1838380ee22f0dcc04c2587
KReysen <43965763+KReysen@users.noreply.github.com>
2019-02-15 17:02:26 -0700

    added authentication popup

--- src/components/User.js
+++ src/components/User.js
@@ -2,15 +2,42 @@ import React, { Component } from 'react';
 import '../styles/User.css';
 
 class User extends Component {
-  constructor(props) {
-    super(props)
-  }
 
+signIn() {
+  debugger
+  const provider = new this.props.firebase.auth.GoogleAuthProvider();
+  this.props.firebase.auth().signInWithPopup( provider );
+}
+
+signOut() {
+  this.props.firebase.auth().signOut();
+}
+
+// don't know what actually needs to be rendered here!! 
   render () {
     return (
       <div className='backdrop'>
         <div className='modal'>
-        <h2>Sign in</h2>
+          <form>
+            <input
+              type="text"
+              placeholder="username"
+              >
+            </input>
+            <br />
+            <input
+              type="submit"
+              value="Sign in"
+              onClick={this.signIn()}
+              >
+            </input>
+            <input
+              type="submit"
+              value="Sign out"
+              onClick={this.signOut()}
+              >
+            </input>
+          </form>
         </div>
       </div>
     );
